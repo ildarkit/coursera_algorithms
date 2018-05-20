b550ad60b8c8c6b5d32fd615a465be7825f0e508
ildark-s <shamief@yandex.ru>
2018-05-20 15:25:07 +0300

    w4 sol4 inversions grader pass

--- solutions/w4/4inversions.py
+++ solutions/w4/4inversions.py
@@ -14,26 +14,29 @@ def get_number_of_inversions(a, inv=0):
     return aa, inversions
 
 
-def merge(x, y, inv):
-    b = []
-    len_y = len(y)
-    while x and y:
-        if x[0] <= y[0]:
-            b.append(x.pop(0))
+def merge(b, c, inv):
+    d = []
+    i = j = 0
+    len_b = len(b)
+    len_c = len(c)
+    while i < len_b and j < len_c:
+        if b[i] <= c[j]:
+            d.append(b[i])
+            i += 1
         else:
-            inv += 1
-            b.append(y.pop(0))
-    if len(x):
-        inv += len(x) * len_y  # ???
-    if x:
-        b.extend(x)
-    elif y:
-        b.extend(y)
-    return b, inv
+            inv += len(b[i:])
+            d.append(c[j])
+            j += 1
+    while i < len_b:
+        d.append(b[i])
+        i += 1
+    while j < len_c:
+        d.append(c[j])
+        j += 1
+    return d, inv
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     print(get_number_of_inversions(a)[1])
-
